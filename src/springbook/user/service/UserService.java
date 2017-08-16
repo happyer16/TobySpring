f1ca8ab366f3db0c4a0136de40623c28d2eec5b6
taehyun_kim <taehyun_kim2@tmax.co.kr>
2017-08-16 22:47:41 +0900

    5.1.3 - (2) 레벨 업그레이드 함수 & 테스트 생성

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -1,6 +1,10 @@
 package springbook.user.service;
 
+import java.util.List;
+
 import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
 
 public class UserService {
   private UserDao userDao;
@@ -9,5 +13,31 @@ public class UserService {
     this.userDao = userDao;
   }
 
+  public void upgradeLevels() {
+    List<User> users = userDao.getAll();
 
+    for (User user : users) {
+      Boolean changed = false; // 레벨 변화 체크 플래그
+
+      // BAISC 레벨 업그레이드 작업
+      if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+        user.setLevel(Level.SILVER);
+        changed = true;
+      }
+
+      // SILVER 레벨 업그레이드 작업
+      else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+        user.setLevel(Level.GOLD);
+        changed = true;
+      }
+
+      else if (user.getLevel() == Level.GOLD)
+        changed = false;
+      else
+        changed = false;
+
+      if (changed)
+        userDao.update(user);
+    }
+  }
 }
--- src/springbook/user/service/UserServiceTest.java
+++ src/springbook/user/service/UserServiceTest.java
@@ -4,11 +4,19 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/test-applicationContext.xml")
@@ -17,6 +25,41 @@ public class UserServiceTest {
   // Test 대상인 UserService 빈을 제공받을 수 있도록 @Autowired가 붙은 인스턴스 변수를 선언해 줌
   @Autowired
   UserService userService;
+
+  @Autowired
+  UserDao userDao;
+
+  List<User> users;
+
+  @Before
+  public void setUp() {
+    users = Arrays.asList(new User("gyumee", "박성철", "springno1", Level.BASIC, 49, 0),
+        new User("gyumee2", "박성4철", "springno61", Level.BASIC, 50, 0),
+        new User("gyumee3", "박성5철", "springno71", Level.SILVER, 60, 29),
+        new User("leegw700", "이길원", "springno2", Level.SILVER, 60, 30),
+        new User("bumjin", "박범진", "springno3", Level.GOLD, 100, 40));
+  }
+
+  @Test
+  public void upgradeLevels() {
+    userDao.deleteAll();
+
+    for (User user : users)
+      userDao.add(user);
+
+    userService.upgradeLevels();
+
+    checkLevel(users.get(0), Level.BASIC);
+    checkLevel(users.get(1), Level.SILVER);
+    checkLevel(users.get(2), Level.SILVER);
+    checkLevel(users.get(3), Level.GOLD);
+    checkLevel(users.get(4), Level.GOLD);
+  }
+
+  private void checkLevel(User user, Level expectedLevel) {
+    User userUpdate = userDao.get(user.getId());
+    assertThat(userUpdate.getLevel(), is(expectedLevel));
+  }
 
   @Test
   public void bean() {
