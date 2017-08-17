6e6bb7b1773ed348dde8c5f3334252afdf63c0b4
taehyun_kim <taehyun_kim2@tmax.co.kr>
2017-08-17 21:19:22 +0900

    5.1.5 - (1) 코드개선 - 문제점 파악

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -25,6 +25,20 @@ public class UserService {
         changed = true;
       }
 
+      /**
+       * 해당 코드의 문제점
+       * 
+       * 1) if,else if,else 읽기가 불편하다.
+       * 
+       * 1. 현재 레벨 파악 2. 업그레이드 조건을 담는 로직 3. 업그레이드를 위한 작업 4. update를 위한 플래그
+       * 
+       * 위와 같이 관련이 있어 보이지만 성격이 다른것들이 섞여 있음
+       * 
+       * 
+       * 2) Level이 추가된다면?
+       * 
+       * enum도 수정하고 if문은 추가된 만큼 늘어난다. -> 메소드는 점점 복잡해짐
+       */
       // SILVER 레벨 업그레이드 작업
       else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
         user.setLevel(Level.GOLD);
