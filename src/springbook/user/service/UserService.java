85d7f094786fe90acaaba749806d5d8b13e010bb
samsung\USER <happyer16@naver.com>
2017-08-28 23:24:31 +0900

    6.1.1. 비즈니스 로직과 트렌젝션 설정 코드간의 분리

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -13,133 +13,135 @@ import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 public class UserService {
-  private UserDao userDao;
-  private DataSource dataSource;
-  private PlatformTransactionManager transactionManager;
+	private UserDao userDao;
+	private DataSource dataSource;
+	private PlatformTransactionManager transactionManager;
 
-  public void setUserDao(UserDao userDao) {
-    this.userDao = userDao;
-  }
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
 
-  public void setDataSource(DataSource dataSource) {
-    this.dataSource = dataSource;
-  }
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
-  public void setTransactionManager(PlatformTransactionManager transactionManager) {
-    this.transactionManager = transactionManager;
-  }
+	public void setTransactionManager(PlatformTransactionManager transactionManager) {
+		this.transactionManager = transactionManager;
+	}
 
-  public void upgradeLevels() throws Exception {
+	public void upgradeLevels() throws Exception {
 
+		TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
 
-    TransactionStatus status =
-        this.transactionManager.getTransaction(new DefaultTransactionDefinition());
+		try {
+			upgradeLevelsInternal();
+			this.transactionManager.commit(status);// 트랜잭션 커밋
+		} catch (Exception e) {
+			this.transactionManager.rollback(status);// 트랜잭션 롤백
+			throw e;
+		}
+	}
 
+	public void upgradeLevelsInternal() {
+		List<User> users = userDao.getAll();
+		for (User user : users) {
+			if (canUpgradeLevel(user))
+				upgradeLevel(user);
+		}
+	}
 
-    try {
-      List<User> users = userDao.getAll();
-      for (User user : users) {
-        if (canUpgradeLevel(user))
-          upgradeLevel(user);
-      }
-      this.transactionManager.commit(status);// 트랜잭션 커밋
-    } catch (Exception e) {
-      this.transactionManager.rollback(status);// 트랜잭션 롤백
-      throw e;
-    }
-  }
+	public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
+	public static final int MIN_RECOMMEND_FOR_GOLD = 30;
 
-  public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
-  public static final int MIN_RECOMMEND_FOR_GOLD = 30;
+	private boolean canUpgradeLevel(User user) {
+		Level currentLevel = user.getLevel();
 
-  private boolean canUpgradeLevel(User user) {
-    Level currentLevel = user.getLevel();
+		switch (currentLevel) {
+		case BASIC:
+			return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
+		case SILVER:
+			return (user.getRecommend() >= MIN_RECOMMEND_FOR_GOLD);
+		case GOLD:
+			return false;
+		default:
+			throw new IllegalArgumentException("Unknown level : " + currentLevel);
+		}
+	}
 
-    switch (currentLevel) {
-      case BASIC:
-        return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
-      case SILVER:
-        return (user.getRecommend() >= MIN_RECOMMEND_FOR_GOLD);
-      case GOLD:
-        return false;
-      default:
-        throw new IllegalArgumentException("Unknown level : " + currentLevel);
-    }
-  }
+	/**
+	 * upgradeLevel을 따로 두면 좋은 점?
+	 * 
+	 * 만약 나중에 레벨이 오른 걸 사용자에게 알려줘야 하는 로직을 추가할 경우,
+	 * 
+	 * 어디를 수정해야 할 지 쉽게 찾을 수 있음.
+	 * 
+	 * @param user
+	 */
+	protected void upgradeLevel(User user) {
 
-  /**
-   * upgradeLevel을 따로 두면 좋은 점?
-   * 
-   * 만약 나중에 레벨이 오른 걸 사용자에게 알려줘야 하는 로직을 추가할 경우,
-   * 
-   * 어디를 수정해야 할 지 쉽게 찾을 수 있음.
-   * 
-   * @param user
-   */
-  protected void upgradeLevel(User user) {
+		/**
+		 * user로 upgradeLevel을 옮긴 이유 : 사용자 정보를 담고 있는 단순한 자바빈이지만 내부 정보를 다루는 기능이 있을 수 있다.
+		 * UserService가 일일이
+		 */
+		user.upgradeLevel();
+		userDao.update(user);
 
-    /**
-     * user로 upgradeLevel을 옮긴 이유 : 사용자 정보를 담고 있는 단순한 자바빈이지만 내부 정보를 다루는 기능이 있을 수 있다. UserService가 일일이
-     */
-    user.upgradeLevel();
-    userDao.update(user);
+		// 기존 코드의 문제점 : 다음 레벨도 알아야 되고... Gold 예외 처리도 없고.... 레벨이 많아진다면 if문도 많아진다.
+		// if (user.getLevel() == Level.BASIC)
+		// user.setLevel(Level.SILVER);
+		// else if (user.getLevel() == Level.SILVER)
+		// user.setLevel(Level.GOLD);
+		// userDao.update(user);
 
-    // 기존 코드의 문제점 : 다음 레벨도 알아야 되고... Gold 예외 처리도 없고.... 레벨이 많아진다면 if문도 많아진다.
-    // if (user.getLevel() == Level.BASIC)
-    // user.setLevel(Level.SILVER);
-    // else if (user.getLevel() == Level.SILVER)
-    // user.setLevel(Level.GOLD);
-    // userDao.update(user);
+	}
 
-  }
+	public void upgradeLevelsOld() {
+		List<User> users = userDao.getAll();
 
-  public void upgradeLevelsOld() {
-    List<User> users = userDao.getAll();
+		for (User user : users) {
+			Boolean changed = false; // 레벨 변화 체크 플래그
 
-    for (User user : users) {
-      Boolean changed = false; // 레벨 변화 체크 플래그
+			// BAISC 레벨 업그레이드 작업
+			if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+				user.setLevel(Level.SILVER);
+				changed = true;
+			}
 
-      // BAISC 레벨 업그레이드 작업
-      if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
-        user.setLevel(Level.SILVER);
-        changed = true;
-      }
+			/**
+			 * 해당 코드의 문제점
+			 * 
+			 * 1) if,else if,else 읽기가 불편하다.
+			 * 
+			 * 1. 현재 레벨 파악 2. 업그레이드 조건을 담는 로직 3. 업그레이드를 위한 작업 4. update를 위한 플래그
+			 * 
+			 * 위와 같이 관련이 있어 보이지만 성격이 다른것들이 섞여 있음
+			 * 
+			 * 
+			 * 2) Level이 추가된다면?
+			 * 
+			 * enum도 수정하고 if문은 추가된 만큼 늘어난다. -> 메소드는 점점 복잡해짐
+			 * 
+			 * 3) 현재 레벨과 업그레이드 조건을 동시에 비교하고 있다.
+			 */
+			// SILVER 레벨 업그레이드 작업
+			else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+				user.setLevel(Level.GOLD);
+				changed = true;
+			}
 
-      /**
-       * 해당 코드의 문제점
-       * 
-       * 1) if,else if,else 읽기가 불편하다.
-       * 
-       * 1. 현재 레벨 파악 2. 업그레이드 조건을 담는 로직 3. 업그레이드를 위한 작업 4. update를 위한 플래그
-       * 
-       * 위와 같이 관련이 있어 보이지만 성격이 다른것들이 섞여 있음
-       * 
-       * 
-       * 2) Level이 추가된다면?
-       * 
-       * enum도 수정하고 if문은 추가된 만큼 늘어난다. -> 메소드는 점점 복잡해짐
-       * 
-       * 3) 현재 레벨과 업그레이드 조건을 동시에 비교하고 있다.
-       */
-      // SILVER 레벨 업그레이드 작업
-      else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
-        user.setLevel(Level.GOLD);
-        changed = true;
-      }
+			else if (user.getLevel() == Level.GOLD)
+				changed = false;
+			else
+				changed = false;
 
-      else if (user.getLevel() == Level.GOLD)
-        changed = false;
-      else
-        changed = false;
+			if (changed)
+				userDao.update(user);
+		}
+	}
 
-      if (changed)
-        userDao.update(user);
-    }
-  }
-
-  public void add(User user) {
-    if (user.getLevel() == null)
-      user.setLevel(Level.BASIC);
-    userDao.add(user);
-  }
+	public void add(User user) {
+		if (user.getLevel() == null)
+			user.setLevel(Level.BASIC);
+		userDao.add(user);
+	}
 }
